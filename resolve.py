f2be06166283dd981b1199be7799b6e29ff2343c
bgt <bgt@sci.fi>
2025-04-20 18:55:50 +0300

    Now each game played is different.

--- resolve.py
+++ resolve.py
@@ -1,20 +1,24 @@
 import pygame
 from grid import Grid
 from pygame.math import Vector2
+import random
 
 width = 600
 height = 400
 resolution_grid = Grid()
 ticker = 0
+line_count = 0
 
 # Run the game
 pygame.init()
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 running = True
+font = pygame.font.SysFont('Arial', 10)
 
 while running:
 
+    opening = False
     x = None
     y = None
     x2 = None
@@ -23,39 +27,56 @@ while running:
         x = target[0]
         y = target[1]
         if resolution_grid.is_valid_line(x, y, 0, -1):
+            opening = True
             x2 = 0
             y2 = -1
-            break
+            if random.choice([True, False]):
+                break
         if resolution_grid.is_valid_line(x, y, 1, -1):
+            opening = True
             x2 = 1
             y2 = -1
-            break
+            if random.choice([True, False]):
+                break
         if resolution_grid.is_valid_line(x, y, 1, 0):
+            opening = True
             x2 = 1
             y2 = 0
-            break
+            if random.choice([True, False]):
+                break
         if resolution_grid.is_valid_line(x, y, 1, 1):
+            opening = True
             x2 = 1
             y2 = 1
-            break
+            if random.choice([True, False]):
+                break
         if resolution_grid.is_valid_line(x, y, 0, 1):
+            opening = True
             x2 = 0
             y2 = 1
-            break
+            if random.choice([True, False]):
+                break
         if resolution_grid.is_valid_line(x, y, -1, 1):
+            opening = True
             x2 = -1
             y2 = 1
-            break
+            if random.choice([True, False]):
+                break
         if resolution_grid.is_valid_line(x, y, -1, 0):
+            opening = True
             x2 = -1
             y2 = 0
-            break
+            if random.choice([True, False]):
+                break
         if resolution_grid.is_valid_line(x, y, -1, -1):
+            opening = True
             x2 = -1
             y2 = -1
-            break
+            if random.choice([True, False]):
+                break
     if x2 is not None:
         resolution_grid.add_line(x, y, x2, y2)
+        line_count += 1
 
     # Poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -92,8 +113,19 @@ while running:
                          end_pos=Vector2(((x2-resolution_grid.x_min)*scale)+5+x_offset, 
                                             ((y2-resolution_grid.y_min)*scale)+5+y_offset))
 
+    punctuator = "!"
+    if opening:
+        punctuator = "..."        
+    text = font.render(f'Line: {line_count}{punctuator}', True, "black")
+    screen.blit(text, (1, 1))
+
     # flip() the display to put your work on screen
     pygame.display.flip()
+
+    while not opening and running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
     clock.tick(60)  # limits FPS to 60?
     ticker += 1
